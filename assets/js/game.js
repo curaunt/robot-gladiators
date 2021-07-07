5f1e218c31aa0aacba4a18c017eb7af8ac747371
curauny <curaunthompson@gmail.com>
2021-07-06 18:06:33 -0700

    setupjs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,23 @@
-// Player data 
 var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-//what is this? 
-console.log(playerName)
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50; 
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
 
 // this creates a function named "fight"
 
-function fight() {
+//function fight() {
     window.alert("The fight has begun!");
   }
-
-// fight();
 
